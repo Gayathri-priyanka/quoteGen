41e6ffc12a3454d742fe24f6b132ce3da5ebb57e
Gayathri-priyanka <gayathripriyankanamburu1@gmail.com>
2024-05-04 17:56:39 -0400

    populating quotes and making buttons work

--- script.js
+++ script.js
@@ -1,12 +1,46 @@
+const quoteContainer=document.getElementById('quote-container');
+const quoteText=document.getElementById('quote');
+const authorText=document.getElementById('author');
+const twitterBtn=document.getElementById('twitter');
+const newQuoteBtn=document.getElementById('new-quote');
+const loader=document.getElementById('loader');
 let apiQuotes=[];
 
+//Show loading
+
+function loading(){
+    loader.hidden=false;
+    quoteContainer.hidden=true;
+}
+function complete(){
+    quoteContainer.hidden=false;
+    loader.hidden=true;
+}
 //function new quote
 function newQuote(){
+    loading();
     const quote=apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-    console.log(quote);
+    //if author name is  niot present replace it with 'unknown string'
+    if(!quote.author){
+        authorText.textContent='Unknown';
+    }
+    else{
+        authorText.textContent=quote.author;
+    }
+    //change font size based on quote length
+    if(quote.text.length>130){
+        quoteText.classList.add('long-quote');
+    }
+    else{
+        quoteText.classList.remove('long-quote');
+    }
+    //set quote, hide loader
+    quoteText.textContent=quote.text;
+    complete();
 }
 // get quotes from API
 async function getQuotes(){
+    
     const apiURL="https://jacintodesign.github.io/quotes-api/data/quotes.json";
     try{
         const response=await fetch(apiURL);
@@ -17,7 +51,13 @@ async function getQuotes(){
         //catch error here
     }
 }
-
-
+//tweet quote
+function tweetQuote(){
+    const twitterUrl=`https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterUrl,'_blank');
+}
+//Event Listeners
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click',tweetQuote);
 //onload 
 getQuotes();
